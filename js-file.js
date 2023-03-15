37a34e980695851db55b96dc62ece99330fe3740
Righteous1ice <hoplabradley@gmail.com>
2023-03-15 22:04:04 +0000

    Fixed the path of javascript file. commented out all previous attempts. Created a series of 16 veritcal cells but no grid yet.

--- js-file.js
+++ js-file.js
@@ -1,4 +1,4 @@
-const container = document.getElementById("container")
+/*const container = document.getElementById("container")
 
 function createGrid(x){
     for (var rows = 0; rows < x; rows++ ) {
@@ -9,4 +9,32 @@ function createGrid(x){
     $(".grid").width(960/x);
     $(".grid").height(960/x);
 
-}
+}*/
+
+const gridSize = 16;
+const cellSize = 50;
+
+const grid = document.getElementById("grid");
+ for (let i = 0; i < gridSize * gridSize; i++){
+    
+
+    
+    const cell = document.createElement("div");
+    cell.style.width = `${cellSize}px`;
+    cell.style.height = `${cellSize}px`;
+    cell.style.border = "1px solid black";
+    
+
+    grid.appendChild(cell);
+    
+ }
+
+ grid.style.display = 'grid';
+ grid.style.gridTemplateColumns = `repeat(${gridSize}, ${cellSize})`;
+ grid.style.gridTemplateRows = `repeat(${gridSize}, ${cellSize})`;
+
+
+//var grid = document.getElementById("grid");
+//var cell = document.getElementById("div");
+//cell.innerHTML ="TEXT";
+//grid.appendChild("cell");
