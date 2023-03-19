bc65735fe05872d07b0ab284b217210c7f2c3338
Righteous1ice <hoplabradley@gmail.com>
2023-03-19 21:04:28 +0000

    The grid is now ammended by the popup input however the css is broken on the popup buttons and the grid does not stay the same px size

--- js-file.js
+++ js-file.js
@@ -1,48 +1,78 @@
-const gridSize = 16;
-const cellSize = 20;
+let gridSize = 16;
+let cellSize = 20;
 
 const grid = document.getElementById("grid");
- for (let i = 0; i < gridSize * gridSize; i++){
-    
-    
 
-    
+
+for (let i = 0; i < gridSize * gridSize; i++) {
+  const cell = document.createElement("div");
+  cell.classList.add("cell");
+  cell.style.width = `${cellSize}px`;
+  cell.style.height = `${cellSize}px`;
+  cell.style.border = "1px solid black";
+  grid.appendChild(cell);
+}
+
+
+jQuery(".cell").hover(function() {
+  jQuery(this).css("background-color", "#3882F6");
+});
+
+
+function updateGridSize() {
+  const width = parseInt(document.querySelector('input[name="width"]').value);
+  const height = parseInt(document.querySelector('input[name="height"]').value);
+  if (!width || !height) return;
+  gridSize = Math.min(width, height);
+  cellSize = Math.floor(600 / gridSize);
+  grid.style.gridTemplateColumns = `repeat(${gridSize}, ${cellSize}px)`;
+  grid.style.gridTemplateRows = `repeat(${gridSize}, ${cellSize}px)`;
+  grid.innerHTML = "";
+  for (let i = 0; i < gridSize * gridSize; i++) {
     const cell = document.createElement("div");
-    cell.classList.add("cell"); // Targets the individual cell
+    cell.classList.add("cell");
     cell.style.width = `${cellSize}px`;
     cell.style.height = `${cellSize}px`;
     cell.style.border = "1px solid black";
-    
-
     grid.appendChild(cell);
-    
-    
- }
-
- jQuery(".cell").hover(function(){ // calls the cell
+  }
+  jQuery(".cell").hover(function() {
     jQuery(this).css("background-color", "#3882F6");
- });
+  });
+}
 
- function openPopup() {
-   document.getElementById("myPopup").classList.add("popup-open");
-   popup.style.display = "block";
-   
- }
+
+function openPopup() {
+  document.getElementById("myPopup").classList.add("popup-open");
+}
+
+
+function closePopup() {
+  document.getElementById("myPopup").classList.remove("popup-open");
+}
+
+
+document.querySelector('form').addEventListener('submit', function(event) {
+  event.preventDefault();
+  updateGridSize();
+  closePopup();
+});
+
+
+
+
  
- function closePopup() {
-   document.getElementById("myPopup").classList.remove("popup-open");
-   popup.style.display = "none";
- }
 
+
+ //input will change grid size 
+ //but not the px size of the grid
+
+ // the grid size and cell size might need
+ // changing as 
 
 //document.getElementById("click me").onclick = function click(){
 
 //}
-
-
-
-
-
 
 
 
